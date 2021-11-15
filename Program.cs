4d4a2b7bc58e66132b6a3ec14bf9831bb39bcb99
Chris Helmich <kagekirin@gmail.com>
2021-11-15 16:15:37 +0900

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_version_bump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
